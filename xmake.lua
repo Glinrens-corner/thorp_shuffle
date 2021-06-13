540e4bb82943aa3d702e0418f4ad4f72a0d3642d
glinren <glinren.kre@gmail.com>
2021-06-13 14:31:52 +0200

    factored out the library use.

--- xmake.lua
+++ xmake.lua
@@ -5,11 +5,12 @@ add_requires("libsodium")
 
 target("example")
     set_kind("binary")
+    set_default(false)
     set_languages("cxx17")
-    add_files("src/*.cpp",
-              "example/*.cpp"   )
+    add_files("example/*.cpp"   )
     add_includedirs("include")
     add_packages("libsodium")
+    add_deps("static_lib")
     if is_plat("windows")then
         add_cxxflags("/permissive-","/W4")
     end
@@ -19,14 +20,26 @@ target("example")
 
 target("test")
     set_kind("binary")
+    set_default(false)
     set_languages("cxx17")
-    add_files("src/*.cpp",
-              "test/*.cpp")
+    add_files("test/*.cpp")
     add_includedirs("include")
     add_packages("libsodium", "doctest")
+    add_deps("static_lib")
     if is_plat("windows")then
         add_cxxflags("/permissive-","/W4")
     end
     after_build(function( target)
         os.cp(target:targetfile(), "$(projectdir)/bin/")
     end)
+
+target("static_lib")
+    set_kind("static")
+    set_default(true)
+    set_languages("cxx17")
+    add_files("src/*.cpp")
+    add_includedirs("include")
+    add_packages("libsodium")
+    if is_plat("windows")then
+        add_cxxflags("/permissive-","/W4")
+    end
