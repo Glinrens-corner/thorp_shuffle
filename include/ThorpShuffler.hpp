7f25e8ed80896c025635f7f6deb4041b15849b68
glinren <glinren.kre@gmail.com>
2021-06-17 21:01:02 +0200

    added comments

--- include/ThorpShuffler.hpp
+++ include/ThorpShuffler.hpp
@@ -10,11 +10,18 @@
 #include <functional>
 #include <sodium.h>
 
+//before trying to understand this code, please read the paper by
+//  Ben Morris, Phillip Rogawayand and Till Stegers "How to Encipher Messages on a Small Domain" 2009.
+//https://doi.org/10.1007/978-3-642-03356-8_17
+// especially chaper 1 and 5 are assumed as known.
+
 namespace thorp {
+    // likely identical to std::byte_t;
     using byte_t = unsigned char;
 
     constexpr inline uint64_t nrounds_per_pass(uint64_t max_message)noexcept;
 
+    // Old implementation. 
     class ThorpObfuscator {
     public:
         ThorpObfuscator(std::vector<byte_t> round_keys_data, uint64_t max_message, uint64_t npasses);
@@ -23,7 +30,6 @@ namespace thorp {
         uint64_t encrypt(uint64_t plaintext)const;
         uint64_t decrypt(uint64_t cyphertext) const;
     private:
-
         static constexpr std::array<byte_t, sizeof(uint64_t)> generate_message(uint64_t);
         static bool generate_random_bit(uint64_t, const byte_t*, unsigned long long)noexcept;
     private:
@@ -33,10 +39,14 @@ namespace thorp {
     };
 
     constexpr inline uint64_t calculate_optimization_level_max() {
+        // libsodium uses blake2d wich generates up to 512 bits.
         static_assert(crypto_generichash_BYTES_MAX * CHAR_BIT == 512, "unexpected hash length");
+        //the largest number such that k*2^(k-1) <= bits in the hash.
+        // see the paper chaper 5.
         return 7;
     };
 
+    // more newerimplementation, incorporates the "5x" trick (which is here a up to 7x trick).
     class OptThorpObfuscator {
         static constexpr uint64_t hash_size_ = crypto_generichash_BYTES_MAX;
     public:
@@ -68,7 +78,7 @@ namespace thorp{
         };
         return message_out;
     }
-
+    // calculate the minimum number of bytes the ThorpObfuscator requires upon instanciation.
     inline constexpr uint64_t ThorpObfuscator::round_keys_data_size(uint64_t npasses, uint64_t max_message)
     {
         const uint64_t nrounds = nrounds_per_pass(max_message) * npasses;
@@ -76,6 +86,7 @@ namespace thorp{
         return nroundkeys_bytes_sum;
     }
 
+    // calculate the minimum number of bytes the OptThorpObfuscator requires upon instanciation.
     constexpr inline uint64_t OptThorpObfuscator::round_keys_data_size(uint64_t npasses, uint64_t max_message, uint64_t optimization_level)
     {
         const uint64_t nrounds = nrounds_per_pass(max_message) * npasses;
@@ -85,7 +96,8 @@ namespace thorp{
     }
     
     
-
+    // calculates the number of rounds needed per pass.
+    // ceil(log_2(N_domain))
     constexpr inline uint64_t nrounds_per_pass(uint64_t max_message)noexcept
     {
 
